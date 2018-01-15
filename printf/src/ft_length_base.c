d88c6c3d21364464dd52dcc1e7bf06d29df0a93d
Gautier VANNESTE <gvannest@e1r5p5.42.fr>
2018-01-15 17:17:19 +0100

    printf

---
+++ printf/src/ft_length_base.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_length.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvannest <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/15 12:56:51 by gvannest          #+#    #+#             */
+/*   Updated: 2018/01/15 17:14:52 by gvannest         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static void	ft_base(t_lst *p)
+{
+	if (TYPE == 'd' || TYPE == 'D' || TYPE == 'i' || TYPE == 'u' || TYPE == 'U')
+		BASE = 10;
+	else if (TYPE == 'o' || TYPE == 'O')
+		BASE = 8;
+	else if (TYPE == 'x' || TYPE == 'X')
+		BASE = 16;
+}
+
+
+void	ft_length(t_lst *p)
+{
+	if (TYPE == 's' || TYPE == 'S')
+		LEN_PARAM = ft_strlen((char*)PARAM);
+	else if (TYPE == 'c' || TYPE == 'C')
+		LEN_PARAM = 1;
+	else if (TYPE == 'p')
+		LEN_PARAM = 14;
+	else
+	{
+		ft_base(p);
+	if (TYPE == 'd' || TYPE == 'D' || TYPE == 'i')
+		LEN_PARAM = ft_strlen(ft_itoabase_u(*(intmax_t*)PARAM, BASE));
+	else	
+		LEN_PARAM = ft_strlen(ft_itoabase_u(*(uintmax_t*)PARAM, BASE));
+	}
+}
