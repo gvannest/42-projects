291edb9b69fe221ac7ba08145b4680b4fb9ad372
Gautier VANNESTE <gvannest@e1r6p2.42.fr>
2018-02-14 11:57:10 +0100

    printf clean

--- printf/src/ft_typec.c
+++ printf/src/ft_typec.c
@@ -6,7 +6,7 @@
 /*   By: gvannest <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 16:27:41 by gvannest          #+#    #+#             */
-/*   Updated: 2018/02/10 12:44:50 by gvannest         ###   ########.fr       */
+/*   Updated: 2018/02/14 10:28:37 by gvannest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ static void		ft_width(t_lst *p)
 	char	t;
 
 	t = ' ';
-	(FLAGS & ZERO ? t = '0' : t);
 	i = WIDTH - 1;
+	(FLAGS & ZERO ? t = '0' : t);
 	while (i > 0)
 	{
 		ft_buffer(t, p);
@@ -35,10 +35,10 @@ int				ft_type_c(t_lst *p, va_list ap)
 	i = 0;
 	if (SIZE & L || SIZE & LL)
 		return (ft_type_unicodec(p, ap));
-	c = ft_cast_char(p, ap);
+	c = (char)ft_cast_char(p, ap);
 	if (!(FLAGS & MINUS))
 		ft_width(p);
-	ft_buffer((char)c, p);
+	ft_buffer(c, p);
 	if (FLAGS & MINUS)
 		ft_width(p);
 	return (0);
